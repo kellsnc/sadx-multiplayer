fd2c9037e0f2d70bb41f9f800f44359ea1a513e2
kellsnc <kellspam@outlook.fr>
2021-12-16 15:37:33 +0100

    Use symbol names, cleanup

--- sadx-multiplayer/splitscreen.cpp
+++ sadx-multiplayer/splitscreen.cpp
@@ -5,54 +5,64 @@
 #include <d3d8.h>
 
 #define TARGET_DYNAMIC(name) ((decltype(name##_r)*)name##_t->Target())
+
+VoidFunc(DisplayTask, 0x40B540);
 DataPointer(IDirect3DDevice8*, Direct3D_Device, 0x03D128B0);
+DataPointer(D3DVIEWPORT8, Direct3D_ViewPort, 0x03D12780);
+
 static int player_count = 2;
 
-Trampoline* DisplayAllObjects_t = nullptr;
-Trampoline* RunAllObjects_t = nullptr;
+Trampoline* DisplayTask_t = nullptr;
+Trampoline* LoopTask_t = nullptr;
 
-void __cdecl DisplayAllObjects_r();
+void __cdecl DisplayTask_r();
+
+void ChangeViewPort(int x, int y, int w, int h)
+{
+    Direct3D_ViewPort = { (unsigned long)x, (unsigned long)y, (unsigned long)w, (unsigned long)h, 0.0f, 1.0f };
+    Direct3D_Device->SetViewport(&Direct3D_ViewPort);
+}
 
 void DrawTwoScreens()
 {
-    D3DVIEWPORT8 leftscreen = { 0, 0, HorizontalResolution / 2, VerticalResolution, 0.0f, 1.0f };
-    D3DVIEWPORT8 rightscreen = { HorizontalResolution / 2, 0, HorizontalResolution / 2, VerticalResolution, 0.0f, 1.0f };
+    ChangeViewPort(0, 0, HorizontalResolution / 2, VerticalResolution);
+    
+    TARGET_DYNAMIC(DisplayTask)();
 
-    Direct3D_Device->SetViewport(&leftscreen);
-
-    TARGET_DYNAMIC(DisplayAllObjects)();
-
-    Direct3D_Device->SetViewport(&rightscreen);
+    ChangeViewPort(HorizontalResolution / 2, 0, HorizontalResolution / 2, VerticalResolution);
 
     // Temporary second camera pos;
     njPushMatrix(_nj_unit_matrix_);
     njTranslate(0, 20, 50, 0); 
     njRotateX(0, 0x8000);
     njRotateY(0, 0x4000);
-    TARGET_DYNAMIC(DisplayAllObjects)();
+    TARGET_DYNAMIC(DisplayTask)();
     njPopMatrixEx();
 }
 
-void __cdecl DisplayAllObjects_r()
+void __cdecl DisplayTask_r()
 {
     if (player_count > 0)
     {
+        // If multiplayer is enabled, split screen:
+
         D3DVIEWPORT8 orig;
         Direct3D_Device->GetViewport(&orig);
 
         DrawTwoScreens();
 
+        Direct3D_ViewPort = orig;
         Direct3D_Device->SetViewport(&orig);
     }
     else
     {
         // Otherwise, normal behaviour:
 
-        TARGET_DYNAMIC(DisplayAllObjects)();
+        TARGET_DYNAMIC(DisplayTask)();
     }
 }
 
-void __cdecl RunAllObjects_r()
+void __cdecl LoopTask_r()
 {
     if (player_count > 0)
     {
@@ -61,21 +71,23 @@ void __cdecl RunAllObjects_r()
         int backup = MissedFrames;
 
         MissedFrames = 1; // Prevent display subs from running
-        TARGET_DYNAMIC(RunAllObjects)();
+        TARGET_DYNAMIC(LoopTask)();
         MissedFrames = backup;
 
-        DisplayAllObjects();
+        DisplayTask();
     }
     else
     {
         // Otherwise, normal behaviour:
 
-        TARGET_DYNAMIC(RunAllObjects)();
+        TARGET_DYNAMIC(LoopTask)();
     }
 }
 
-void SplitScreen_Init() {
-    DisplayAllObjects_t = new Trampoline((int)DisplayAllObjects, (int)DisplayAllObjects + 0x6, DisplayAllObjects_r);
-    RunAllObjects_t = new Trampoline((int)RunAllObjects, (int)RunAllObjects + 0x8, RunAllObjects_r);
-    WriteCall((void*)((int)(RunAllObjects_t->Target()) + 3), RunObjectIndex); // Repair RunAllObjects_t
+void SplitScreen_Init()
+{
+    LoopTask_t = new Trampoline(0x40B170, 0x40B178, LoopTask_r);
+    WriteCall((void*)((int)(LoopTask_t->Target()) + 3), RunObjectIndex); // Repair LoopTask_t
+    DisplayTask_t = new Trampoline(0x40B540, 0x40B546, DisplayTask_r);
+    WriteCall((void*)((int)(LoopTask_t->Target()) + 3), (void*)0x40B0C0); // Repair DisplayTask_t
 }
