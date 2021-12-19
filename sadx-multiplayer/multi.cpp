0bd0245f23a5d7b75b4db4eb33b0d5587bdbfd18
Sora <sora.x@hotmail.fr>
2021-12-19 02:37:34 +0100

    Menu Multi working for multiple players

--- sadx-multiplayer/multi.cpp
+++ sadx-multiplayer/multi.cpp
@@ -3,10 +3,18 @@
 
 extern bool MultiMenuEnabled;
 
-uint8_t cursor = 0;
+uint8_t cursor[8];
 
 NJS_TEXNAME multicharTex[10];
 NJS_TEXLIST multichar_Texlist = { arrayptrandlength(multicharTex) };
+
+enum playReadyState {
+	disconnected,
+	pressedStart,
+	ready
+};
+
+int playerReady[8];
 
 enum CharacterMenu {
 	sonicIcon,
@@ -48,14 +56,95 @@ NJS_POINT2 cursorPosArray[8]{
 };
 
 NJS_SPRITE MultiTexSprite = { { 0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim };
-NJS_SPRITE MultiCursorSprite = { { 0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim };
 
-void DrawCursor() {
+NJS_SPRITE MultiCursorSprite[8] = {
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
+	{ {0, 0, 0 }, 1.0f, 1.0f, 0, &multichar_Texlist, MultiTexAnim },
 
-	MultiCursorSprite.p.x = cursorPosArray[cursor].x;
-	MultiCursorSprite.p.y = cursorPosArray[cursor].y;
+};
 
-	njDrawSprite2D_DrawNow(&MultiCursorSprite, cursorIcon +1, -499, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR);
+void DrawCursor(char pnum) {
+
+	MultiCursorSprite[pnum].p.x = cursorPosArray[cursor[pnum]].x;
+	MultiCursorSprite[pnum].p.y = cursorPosArray[cursor[pnum]].y;
+
+	njDrawSprite2D_DrawNow(&MultiCursorSprite[pnum], cursorIcon + 1, -499, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR);
+}
+
+struct MenuCharStruct {
+	NJS_TEXLIST texture;
+	NJS_ACTION* action;
+};
+
+DataPointer(NJS_ACTION, stru_8F83F4, 0x8F83F4);
+DataPointer(NJS_ACTION, stru_89E254, 0x89E254);
+
+MenuCharStruct Multi_MenuCharArray[]{
+
+	{ SONIC_TEXLIST, SONIC_ACTIONS[0]},
+	{ EGGMAN_TEXLIST, &stru_89E254 },
+	{ MILES_TEXLIST, MILES_ACTIONS[0] },
+	{ KNUCKLES_TEXLIST, KNUCKLES_ACTIONS[0]},
+	{ TIKAL_TEXLIST,  &stru_8F83F4 },
+	{ AMY_TEXLIST, AMY_ACTIONS[0]},
+	{ E102_TEXLIST, E102_ACTIONS[0] },
+	{ BIG_TEXLIST, BIG_ACTIONS[0]  },
+
+};
+
+
+bool isEveryoneReady() {
+
+	char countRDY = 0;
+	char countNotRDY = 0;
+
+	for (uint8_t i = 0; i < 8; i++) {
+
+		if (playerReady[i] == ready)
+			countRDY++;
+
+		if (playerReady[i] == pressedStart)
+			countNotRDY++;
+	}
+
+	if (countNotRDY <= 0 && countRDY >= 1)
+		return true;
+
+	return false;
+}
+
+
+
+
+void StartMulti_Game(TrialActSelWk* wk) {
+
+	if (!isEveryoneReady())
+		return;
+
+	LastLevel = CurrentLevel;
+	LastAct = CurrentAct;
+
+	CurrentCharacter = cursor[0];
+	CurrentLevel = 1;
+	CurrentAct = 0;
+
+
+	SeqTp->awp[1].work.ul[0] = 100;
+
+	AvaStgActT stgact = { CurrentLevel, CurrentAct };
+	AvaCmnPrm = stgact;
+	AdvertiseWork.Stage = stgact.Stg;
+	AdvertiseWork.Act = stgact.Act;
+	wk->SelStg = stgact.Stg;
+
+	wk->Stat = ADVA_STAT_FADEOUT;
+	wk->T = 0.0f;
 }
 
 void __cdecl MultiMenuExec_Display(task* tp)
@@ -77,9 +166,10 @@ void __cdecl MultiMenuExec_Display(task* tp)
 			SetMaterialAndSpriteColor_Float(1, 1, 1, 1);
 			SetDefaultAlphaBlend();
 
-			DrawCursor();
+
 
 			for (uint8_t i = 0; i < 8; i++) {
+				DrawCursor(i);
 				MultiTexSprite.p.x = cursorPosArray[i].x;
 				MultiTexSprite.p.y = cursorPosArray[i].y;
 				njDrawSprite2D_DrawNow(&MultiTexSprite, i, -500, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR);
@@ -99,32 +189,35 @@ void __cdecl MultiMenuExec_Display(task* tp)
 
 bool MultiMenu_CheckMoveInput(int button, char pNum)
 {
+	if (pNum > 0 && playerReady[pNum] < pressedStart)
+		return;
+
 	if ((PressedButtons[pNum] & button) != 0)
 	{
 
 		switch (button)
 		{
 		case Buttons_Up:
-			cursor -= 4;
+			cursor[pNum] -= 4;
 			break;
 		case Buttons_Down:
-			cursor += 4;
+			cursor[pNum] += 4;
 			break;
 		case Buttons_Left:
-			cursor--;
+			cursor[pNum]--;
 			break;
 		case Buttons_Right:
-			cursor++;
+			cursor[pNum]++;
 			break;
 		default:
 			return false;
 		}
 
-		if (cursor < sonicIcon)
-			cursor = bigIcon;
+		if (cursor[pNum] < sonicIcon)
+			cursor[pNum] = bigIcon;
 
-		if (cursor > msIcon)
-			cursor = sonicIcon;
+		if (cursor[pNum] > msIcon)
+			cursor[pNum] = sonicIcon;
 
 		PlayMenuBipSound();
 
@@ -133,20 +226,33 @@ bool MultiMenu_CheckMoveInput(int button, char pNum)
 	return false;
 }
 
-
 void MultiMenu_InputCheck(task* tp, TrialActSelWk* wk)
 {
-	if (MenuBackButtonsPressed())
-	{
-		CmnAdvaModeProcedure(ADVA_MODE_TITLE_MENU); // force back to lead to menu
-		PlayMenuBackSound();
-		PlayMenuBackSound();
-		wk->Stat = ADVA_STAT_FADEOUT;
-		wk->T = 0.0;
-		return;
-	}
 
 	for (uint8_t i = 0; i < PLAYER_MAX; i++) {
+
+
+		if (MenuBackButtonsPressed_r(i))
+		{
+			CmnAdvaModeProcedure(ADVA_MODE_TITLE_MENU); // force back to lead to menu
+			PlayMenuBackSound();
+			PlayMenuBackSound();
+			wk->Stat = ADVA_STAT_FADEOUT;
+			wk->T = 0.0;
+			return;
+		}
+
+
+		if (MenuSelectButtonsPressed_r(i))
+		{
+			PlayMenuEnterSound();
+
+
+			if (playerReady[i] < ready)
+				playerReady[i]++;
+		}
+
+
 		if (MultiMenu_CheckMoveInput(Buttons_Up, i))
 			return;
 
@@ -158,11 +264,8 @@ void MultiMenu_InputCheck(task* tp, TrialActSelWk* wk)
 
 		if (MultiMenu_CheckMoveInput(Buttons_Right, i))
 			return;
-	}
 
-	if (MenuSelectButtonsPressed())
-	{
-		PlayMenuEnterSound();
+
 	}
 
 	return;
@@ -181,10 +284,12 @@ void __cdecl MultiMenuExec_Main(task* tp)
 
 	if (SeqTp->awp->work.ul[1] == ADVA_MODE_TRIALACT_SEL && wk->Stat == ADVA_STAT_REQWAIT)
 	{
+		memset(playerReady, 0, sizeof(playerReady));
 		MusicList[MusicIDs_Trial].Name = "btl_sel";
 		PlayMenuMusicID(4);
 		PlayVoice(40);
 		LoadPVM("multichar", &multichar_Texlist);
+
 		wk->Stat = ADVA_STAT_FADEIN;
 		wk->T = 0.0f;
 		wk->SelStg = -1;
@@ -215,6 +320,7 @@ void __cdecl MultiMenuExec_Main(task* tp)
 	case ADVA_STAT_KEEP:
 		SeqTp->awp->work.ul[3] = 3;
 		MultiMenu_InputCheck(tp, wk);
+		StartMulti_Game(wk);
 		break;
 	case ADVA_STAT_FADEOUT:
 		wk->T += MissedFrames_B * 0.1f;
--- sadx-multiplayer/utils.h
+++ sadx-multiplayer/utils.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void DrawSADXText(const char* text, __int16 x, __int16 y, int width, int height);
+__int32 MenuSelectButtonsPressed_r(char pnum);
+__int32 MenuBackButtonsPressed_r(char pnum);
