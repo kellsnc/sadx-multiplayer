6009ef1a37a777892f4c0190ca59a428c7689397
kellsnc <kellspam@outlook.fr>
2021-12-23 23:04:51 +0100

    Improve ObjectBeachWave

--- sadx-multiplayer/patches/ObjectBeachWave.cpp
+++ sadx-multiplayer/patches/ObjectBeachWave.cpp
@@ -3,16 +3,15 @@
 
 void __cdecl ObjectBeachWave_act1_disp_j(task* tp)
 {
+	// Draw only if object is coded for current screen id
 	if (tp->twp->btimer == SplitScreen::numScreen)
 	{
 		Obj_EC1Water_Display((ObjectMaster*)tp);
 	}
 }
 
-void __cdecl ObjectBeachWave_act1_exec_j(task* tp)
+void __cdecl ObjectBeachWave_act1_exec_multi(task* tp)
 {
-	Obj_EC1Water_Main((ObjectMaster*)tp);
-
 	auto twp = tp->twp;
 
 	NJS_VECTOR plpos;
@@ -21,6 +20,8 @@ void __cdecl ObjectBeachWave_act1_exec_j(task* tp)
 	twp->pos.z = floorf((plpos.z - twp->pos.z) * 0.04f) * 25.0f + twp->pos.z;
 	*(float*)0x3C5E78C = twp->pos.x;
 	*(float*)0x3C5E788 = twp->pos.z;
+
+	tp->disp(tp);
 }
 
 void __cdecl ObjectBeachWave_act1_r(task* tp);
@@ -28,16 +29,15 @@ Trampoline ObjectBeachWave_act1_t(0x501970, 0X501977, ObjectBeachWave_act1_r);
 void __cdecl ObjectBeachWave_act1_r(task* tp)
 {
 	TARGET_STATIC(ObjectBeachWave_act1)(tp);
-	tp->exec = ObjectBeachWave_act1_exec_j;
 	tp->disp = ObjectBeachWave_act1_disp_j;
 
 	// Load one more per player since movement is calculated in main
 	for (int i = 1; i < player_count; ++i)
 	{
-		auto tp2 = CreateElementalTask(LoadObj_UnknownA | LoadObj_Data1, LEV_6, ObjectBeachWave_act1_exec_j);
+		auto tp2 = CreateElementalTask(LoadObj_UnknownA | LoadObj_Data1, LEV_6, ObjectBeachWave_act1_exec_multi);
 		tp2->twp->btimer = 1;
 		tp2->twp->pos.y = -1.5f;
-		tp2->exec = ObjectBeachWave_act1_exec_j;
+		tp2->exec = ObjectBeachWave_act1_exec_multi;
 		tp2->disp = ObjectBeachWave_act1_disp_j;
 	}
 }
