eeb36eb645bc5d86ca6e25842de30146aaad5585
kellsnc <kellspam@outlook.fr>
2021-12-27 05:03:53 +0100

    Patch skyboxes display

--- sadx-multiplayer/patches.cpp
+++ sadx-multiplayer/patches.cpp
@@ -590,4 +590,12 @@ void InitPatches()
 
 	// EnemySearchPlayer
 	WriteData((char*)0x4CCB3F, (char)PLAYER_MAX);
+
+	// Patch Skyboxes (display function managing mode)
+	WriteData((void**)0x4F723E, (void*)0x4F71A0); // Emerald Coast
+	WriteData((void**)0x4DDBFE, (void*)0x4DDB60); // Windy Valley
+	WriteData((void**)0x61D57E, (void*)0x61D4E0); // Twinkle Park
+	WriteData((void**)0x610A7E, (void*)0x6109E0); // Speed Highway
+	WriteData((void**)0x5E1FCE, (void*)0x5E1F30); // Lost World
+	WriteData((void**)0x4EA26E, (void*)0x4EA1D0); // Ice Cap
 }
