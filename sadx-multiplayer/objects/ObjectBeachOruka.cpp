190e718236b95a7ad303c2e2bb15aca9b5e89f48
kellsnc <kellspam@outlook.fr>
2022-01-31 15:07:22 +0100

    Patch ObjectBeachOruka

---
+++ sadx-multiplayer/objects/ObjectBeachOruka.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+
+void __cdecl ObjectBeachAttackOrukaSummon_r(task* tp);
+void __cdecl ObjectBeachPathOrukaSummon_r(task* tp);
+
+Trampoline ObjectBeachAttackOrukaSummon_t(0x4FBE40, 0x4FBE47, ObjectBeachAttackOrukaSummon_r);
+Trampoline ObjectBeachPathOrukaSummon_t(0x4FBBE0, 0x4FBBE7, ObjectBeachPathOrukaSummon_r);
+
+void __cdecl ObjectBeachAttackOrukaSummon_r(task* tp)
+{
+	if (!multiplayer::IsBattleMode())
+	{
+		TARGET_STATIC(ObjectBeachAttackOrukaSummon)(tp);
+	}
+}
+
+void __cdecl ObjectBeachPathOrukaSummon_r(task* tp)
+{
+	if (!multiplayer::IsBattleMode())
+	{
+		TARGET_STATIC(ObjectBeachPathOrukaSummon)(tp);
+	}
+}
