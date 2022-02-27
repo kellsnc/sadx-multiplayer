c923e6a4210b802963b1b544bf546e935366c57f
kellsnc <kellspam@outlook.fr>
2022-02-27 22:21:23 +0100

    Patch ObjectFinalEggUptarget

---
+++ sadx-multiplayer/objects/ObjectFinalEggUpTarget.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "multiplayer.h"
+
+static void __cdecl uptarget_exec_wait_r(task* tp);
+Trampoline uptarget_exec_wait_t(0x5B5740, 0x5B5747, uptarget_exec_wait_r);
+static void __cdecl uptarget_exec_wait_r(task* tp)
+{
+    if (multiplayer::IsActive())
+    {
+        auto twp = tp->twp;
+
+        if (IsPlayerInSphere(&twp->pos, twp->scl.x))
+        {
+            twp->mode = 1;
+            twp->wtimer = 7;
+        }
+    }
+    else
+    {
+        TARGET_STATIC(uptarget_exec_wait)(tp);
+    }
+}
