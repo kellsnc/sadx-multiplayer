5580e250f389506f84b1a2d7c9017a4221820ad7
Sora <sora.x@hotmail.fr>
2022-03-17 00:55:45 +0100

    milesrace progress

--- sadx-multiplayer/milesrace.cpp
+++ sadx-multiplayer/milesrace.cpp
@@ -8,6 +8,203 @@ DataPointer(int, MRaceStageNumber, 0x3C539EC);
 TaskFunc(InitMoble2PControl, 0x47D8C0);
 TaskFunc(InitSonic2PControl, 0x47D820);
 
+enum Levels2P {
+	Levels2P_WValley,
+	Levels2P_SHighway,
+	Levels2P_SDeck,
+	Levels2P_Icecap,
+	Levels2P_Casinopolis
+};
+
+struct sMRacePath
+{
+	int flag;
+	NJS_POINT3 pos;
+};
+
+struct sSonicCtrl
+{
+	NJS_POINT3 now_path_pos;
+	NJS_POINT3 tgt_path_pos;
+	NJS_POINT3 vec_snc_tgt;
+	float dist_snc_tgt;
+	int path_flag;
+	int path_flag_bak;
+	int last_ang;
+	float pl_last_spd;
+	int jump_cnt;
+};
+
+
+DataPointer(sSonicCtrl, SonicCtrlBuff, 0x3C539F8);
+DataPointer(sMRacePath*, PathTbl_Sonic, 0x03C539F4);
+DataPointer(sMRacePath*, PathTbl_Miles, 0x3C53A64);
+
+auto GetNearestPath = GenerateUsercallWrapper<int (*)(sMRacePath* path_tbl, __int16 max_path, NJS_POINT3* pos)>(rAX, 0x47B7F0, rECX, rBX, noret);
+
+static void SonicAI_Casino_Init(taskwk* twp, taskwk* stwp, taskwk* mtwp)
+{
+	sMRacePath* v6; // r4
+	__int16 v7; // r3
+	__int16 v8; // r4
+	sMRacePath* v9; // r10
+	sMRacePath** v10; // r8
+	sMRacePath* v11; // r10
+	int v12; // ctr
+
+	twp->mode = 1;
+	stwp->ang = mtwp->ang;
+	MiscEntityVector.y = 1.0;
+	MiscEntityVector.z = 10.0;
+	MiscEntityVector.x = 0.0;
+	PConvertVector_P2G(mtwp, &MiscEntityVector);
+	stwp->pos.x = mtwp->pos.x + MiscEntityVector.x;
+	v6 = PathTbl_Miles;
+	stwp->pos.y = mtwp->pos.y + MiscEntityVector.y;
+	stwp->pos.z = mtwp->pos.z + MiscEntityVector.z;
+	twp->timer.w[0] = GetNearestPath(v6, twp->counter.w[0], &mtwp->pos);
+	v7 = GetNearestPath(PathTbl_Sonic, twp->counter.w[1], &stwp->pos);
+	v8 = twp->timer.w[0];
+	v9 = PathTbl_Sonic;
+	twp->timer.w[1] = v7;
+	v10 = &PathTbl_Sonic;
+	v11 = &v9[v8];
+	v12 = 15;
+	do
+	{
+		*++v10 = 0;
+		--v12;
+	} while (v12);
+	SonicCtrlBuff.now_path_pos.x = v11->pos.x;
+	SonicCtrlBuff.now_path_pos.y = v11->pos.y;
+	SonicCtrlBuff.now_path_pos.z = v11->pos.z;
+	SonicCtrlBuff.tgt_path_pos = v11[1].pos;
+	SonicCtrlBuff.path_flag = v11[1].flag;
+}
+
+static void SonicAI_IceCapInit(EntityData1* data, EntityData1* sonicData, EntityData1* milesData)
+{
+	sMRacePath* pathTbl; 
+	float v5; 
+	int max;
+
+	pathTbl = PathTbl_Sonic;
+	data->Action = 1;
+	ForcePlayerAction(sonicData->CharIndex, 44);
+	sonicData->Rotation = milesData->Rotation;
+	MiscEntityVector.x = 0.0;
+	MiscEntityVector.y = 1.0;
+	MiscEntityVector.z = 10.0;
+	PConvertVector_P2G((taskwk*)milesData, &MiscEntityVector);
+	sonicData->Position.x = MiscEntityVector.x + milesData->Position.x;
+	sonicData->Position.y = MiscEntityVector.y + milesData->Position.y;
+	sonicData->Position.z = MiscEntityVector.z + milesData->Position.z;
+	ObjectMaster* board = LoadObject((LoadObj)(LoadObj_Data1 | LoadObj_Data2), 2, Snowboard_Sonic_Load);
+	board->Data1->CharIndex = sonicData->CharIndex;
+	memset(&SonicCtrlBuff, 0, 60u);
+	sMRacePath** v7 = &PathTbl_Sonic;
+	max = 15;
+	do
+	{
+		*++v7 = 0;
+		--max;
+	} while (max);
+
+	SonicCtrlBuff.now_path_pos.x = pathTbl->pos.x;
+	SonicCtrlBuff.now_path_pos.y = pathTbl->pos.y;
+	SonicCtrlBuff.now_path_pos.z = pathTbl->pos.z;
+	SonicCtrlBuff.tgt_path_pos = pathTbl[1].pos;
+	SonicCtrlBuff.path_flag = pathTbl[1].flag;
+}
+
+static void __cdecl Sonic2PAI_Main(ObjectMaster* task)
+{
+	EntityData1* AIptr;
+	EntityData1* data;
+	EntityData1* P1ptr; 
+	CollisionInfo* colInfo;
+	int colMax;
+	int colCount;
+	int sonicAction;
+	data = task->Data1;
+	char pnum = data->CharIndex;
+	AIptr = EntityData1Ptrs[pnum];
+	P1ptr = EntityData1Ptrs[0];
+
+	if (!AIptr || !P1ptr) {
+		CheckThingButThenDeleteObject(task);
+		return;
+	}
+
+	colInfo = EntityData1Ptrs[pnum]->CollisionInfo;
+	colMax = 0;
+
+	if (colInfo->nbInfo)
+	{
+		colCount = 0;
+		do
+		{
+			colInfo->CollisionArray[colCount].damage &= 0xDFu;
+			colInfo = AIptr->CollisionInfo;
+			++colMax;
+			++colCount;
+		} while (colMax < colInfo->nbInfo);
+	}
+
+	sonicAction = data->Action;
+
+	switch (AICourse)
+	{
+		case Levels2P_WValley:
+			if (!data->Action)
+			{
+				goto LABEL_17;
+			}
+			goto LABEL_13;
+		case Levels2P_SHighway:
+			break;
+		case Levels2P_SDeck:
+			if (data->Action)
+			{
+				goto LABEL_13;
+			}
+			goto LABEL_17;
+		case Levels2P_Icecap:
+			if (data->Action)
+			{
+				goto LABEL_13;
+			}
+			SonicAI_IceCapInit(data, AIptr, P1ptr);
+			break;
+		case Levels2P_Casinopolis:
+
+			if (data->Action)
+			{
+			LABEL_13:
+				if (sonicAction == 1)
+				{
+					//SonicAI_Casino_Init(task AIptr, P1ptr);
+				}
+			}
+			else
+			{
+			LABEL_17:
+				SonicAI_Casino_Init((taskwk*)data, (taskwk*)AIptr, (taskwk*)P1ptr);
+			}
+			break;
+		default:
+			CheckThingButThenDeleteObject(task);
+			return;
+	}
+
+	late_SetFunc(
+		(void(__cdecl*)(void*))TailsVS_DrawBarThing,
+		task,
+		22046.5,
+		QueuedModelFlagsB_EnableZWrite);
+	
+}
+
 static void LoadNPCSonicTask(int num)
 {
 	auto tp = CreateElementalTask(0xAu, 0, InitSonic2PControl);
@@ -19,7 +216,7 @@ static void LoadNPCSonicTask(int num)
 	TASKWK_PLAYERID(tp->twp) = num;
 	tp->twp->id = 2;
 
-	PadReadOffP(1u);
+	PadReadOffP(num);
 }
 
 void Set_NPC_Sonic_m(int num)
@@ -66,5 +263,5 @@ void Set_NPC_Sonic_m(int num)
 
 void InitMilesRace()
 {
-	
+
 }
