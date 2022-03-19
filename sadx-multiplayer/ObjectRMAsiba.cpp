2168b48849c2704d0fbda8d7f245684641dec217
Sora <sora.x@hotmail.fr>
2022-03-19 16:02:47 +0100

    RM Asiba done

--- sadx-multiplayer/ObjectRMAsiba.cpp
+++ sadx-multiplayer/ObjectRMAsiba.cpp
@@ -8,15 +8,140 @@ Trampoline ObjectMountainAsiba_t(0x60E150, 0x60E156, ObjectMountainAsiba_r);
 
 static auto Masiba_InitDyncol = GenerateUsercallWrapper<void (*)(task* a1)>(noret, 0x60DDB0, rEDI); // custom name original is "init_9"
 
+static _BOOL1 sub_60DE20(taskwk* a1, char pnum)
+{
+	task* player; 
+	taskwk* playerData;
+	double calcPosX;
+	double calcPosZ;
+	double calcSin;
+	float resultPos; 
+	float calcPosY; 
+
+	player = (task*)GetCharacterObject(pnum);
+
+	if (player)
+	{
+		playerData = player->twp;
+		calcPosX = playerData->pos.x - a1->pos.x;
+		calcPosZ = playerData->pos.z - a1->pos.z;
+		resultPos = calcPosX * calcPosX + calcPosZ * calcPosZ;
+		calcPosY = playerData->pos.y - a1->pos.y;
+		calcSin = njSin(9102) * 50.0;
+		if (resultPos < 2500.0 && fabs(calcPosY) < calcSin)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static void OMasiba_Exec_r(task* tp)
+{
+	taskwk* data;
+	int result;
+	int posPlay;
+	forcewk* fwp;
+	task* player;
+	char pnum;
+	taskwk* playerData;
+
+	int btimer;
+	signed int totalTime;
+	double posX;
+	double calcPosX;
+	double calcPosY;
+	__int64 resultPosPlayerXY;
+	double calcPosYAgain;
+	double calcPosZ;
+	__int64 resultPosPlayerYZ;
+	int calcAngZ;
+
+	data = tp->twp;
+	result = 0;
+	posPlay = 0;
+	fwp = tp->fwp;
+
+	auto ptwp = CCL_IsHitPlayer(data);
+
+	if (!ptwp)
+		return;
+
+	pnum = TASKWK_PLAYERID(ptwp);
+	player = (task*)GetCharacterObject(pnum);
+	playerData = player->twp;
+
+	if (!rd_mountain_twp || rd_mountain_twp->scl.y == 0.0 && rd_mountain_twp->counter.f == 0.0)
+		fwp->pos_spd.y = 0.0;
+	else
+		fwp->pos_spd.y = rd_mountain_twp->counter.f;
+
+	if (sub_60DE20(data, pnum))
+	{
+		btimer = data->btimer;
+
+		if (!data->btimer)
+			data->value.f = -0.035;
+
+		totalTime = btimer + 1;
+
+		if (200 / totalTime >= 1)
+			data->btimer = totalTime;
+		else
+			data->value.f = 0.0;
+
+		posX = data->pos.x;
+
+		data->wtimer += 200 / totalTime;
+
+		calcPosX = playerData->pos.x - (float)posX;
+		calcPosY = playerData->pos.y - data->pos.y;
+
+		if (calcPosX <= 0.0)
+			(resultPosPlayerXY) = (int)(atan2(fabs(calcPosX), calcPosY) * 65536.0 * 0.1591549762031479);
+		else
+			(resultPosPlayerXY) = (int)(atan2(calcPosX, calcPosY) * 65536.0 * -0.1591549762031479);
+
+		posPlay = (int)(float)((float)resultPosPlayerXY * (float)0.083333336);
+
+		calcPosZ = playerData->pos.z - data->pos.y;
+		calcPosYAgain = playerData->pos.y - data->pos.y;
+
+		if (calcPosZ <= 0.0)
+			(resultPosPlayerYZ) = (int)(atan2(fabs(calcPosZ), calcPosYAgain) * 65536.0 * -0.1591549762031479);
+		else
+			(resultPosPlayerYZ) = (int)(atan2(calcPosZ, calcPosYAgain) * 65536.0 * 0.1591549762031479);
+
+		result = (int)(float)((float)resultPosPlayerYZ * (float)0.083333336);
+	}
+	else
+	{
+		data->btimer = 0;
+		data->wtimer = 0;
+	}
+
+	data->ang.x = AdjustAngle(data->ang.x, 2 * result, 15);
+	calcAngZ = AdjustAngle(data->ang.z, 2 * posPlay, 15);
+	data->ang.z = calcAngZ;
+	data->pos.y += fwp->pos_spd.y;
+	WithdrawCollisionEntry(tp, (obj*)data->counter.l);
+	ReleaseMobileLandObject((obj*)data->counter.l);
+	Masiba_InitDyncol(tp);
+
+	if (CheckCollisionP(&data->pos, 90.0))
+		data->flag |= 0x100u;
+	else
+		data->flag &= 0xFEFFu;
+}
+
 static void __cdecl ObjectMountainAsiba_r(task* obj)
 {
-
 	if (!multiplayer::IsActive())
 	{
 		TARGET_STATIC(ObjectMountainAsiba);
 		return;
 	}
-
 
 	if (CheckRangeOut(obj))
 		return;
@@ -27,7 +152,7 @@ static void __cdecl ObjectMountainAsiba_r(task* obj)
 	{
 		if (data->mode == 1 && !ObjectSelectedDebug((ObjectMaster*)obj))
 		{
-			//OMasiba_Exec(obj);
+			OMasiba_Exec_r(obj);
 			OMasiba_Display(obj);
 			return;
 		}
@@ -38,6 +163,7 @@ static void __cdecl ObjectMountainAsiba_r(task* obj)
 		obj->disp = OMasiba_Display;
 		Masiba_InitDyncol(obj);
 		data->mode = 1;
+
 		if (rd_mountain_twp)
 		{
 			if (!ObjectSelectedDebug((ObjectMaster*)obj))
@@ -48,5 +174,4 @@ static void __cdecl ObjectMountainAsiba_r(task* obj)
 	}
 
 	OMasiba_Display(obj);
-
 }
