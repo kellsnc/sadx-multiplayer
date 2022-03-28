e904503f5d836501bebc3dbf7017446333a60dab
kellsnc <kellspam@outlook.fr>
2022-03-28 21:34:03 +0200

    Patch ObjectTPDoubleDoor

--- sadx-multiplayer/objects/o_twinkle_door.cpp
+++ sadx-multiplayer/objects/o_twinkle_door.cpp
@@ -3,9 +3,11 @@
 
 static void checkTPDoorMode_w();
 static void checkTPDoorMode2_w();
+static void __cdecl ObjectTPDoubleDoor_r(task* tp);
 
 Trampoline checkTPDoorMode_t(0x61E460, 0x61E465, checkTPDoorMode_w);
 Trampoline checkTPDoorMode2_t(0x61E510, 0x61E515, checkTPDoorMode2_w);
+Trampoline ObjectTPDoubleDoor_t(0x61EAC0, 0x61EAC6, ObjectTPDoubleDoor_r);
 
 static void getModeWithFOV(taskwk* twp, taskwk* ptwp)
 {
@@ -107,5 +109,111 @@ static void __declspec(naked) checkTPDoorMode2_w()
 		call checkTPDoorMode2_r
 		pop esi
 		retn
+	}
+}
+
+static void ObjectTPDoubleDoor_m(task* tp)
+{
+	if (CheckRangeOutWithR(tp, 1020100.0f))
+	{
+		return;
+	}
+
+	auto twp = tp->twp;
+
+	switch (twp->mode)
+	{
+	case 0i8:
+		twp->mode = 1i8;
+
+		twp->scl.z = 0.0f;
+		twp->scl.y = 0.0f;
+
+		if (twp->scl.x <= 0.0f)
+		{
+			if (twp->scl.x == 0.0f)
+			{
+				twp->smode = 1i8;
+			}
+			else
+			{
+				twp->mode = 3i8;
+			}
+		}
+		else
+		{
+			twp->smode = 0i8;
+		}
+
+		twp->counter.ptr = (void*)0x027AF5EC;
+		tp->disp = (TaskFuncPtr)0x61E910;
+		CCL_Init(tp, (CCL_INFO*)0x27C5BD0, 4, 4u);
+		SET_COLLI_RANGE(twp->cwp, 80.0f);
+		twp->cwp->info[1].attr |= 0x10u;
+		twp->cwp->info[2].attr |= 0x10u;
+		twp->cwp->info[3].attr |= 0x10u;
+		break;
+	case 1i8:
+		if (twp->smode)
+		{
+			if (twp->smode != 1)
+			{
+				twp->mode = 3i8;
+			}
+			else
+			{
+				if (IsPlayerInSphere(&twp->pos, 50.0f))
+				{
+					twp->mode = 2i8;
+					twp->cwp->info->attr |= 0x10u;
+					twp->cwp->info[1].attr &= ~0x10u;
+					twp->cwp->info[2].attr &= ~0x10u;
+					twp->cwp->info[3].attr &= ~0x10u;
+				}
+			}
+		}
+		else if (IsSwitchPressed(0) && IsSwitchPressed(1) && IsSwitchPressed(2))
+		{
+			twp->mode = 2i8;
+			twp->cwp->info->attr |= 0x10u;
+			twp->cwp->info[1].attr &= ~0x10u;
+			twp->cwp->info[2].attr &= ~0x10u;
+			twp->cwp->info[3].attr &= ~0x10u;
+		}
+
+		twp->value.l = AdjustAngle(twp->value.l, 0, 256);
+		EntryColliList(twp);
+		break;
+	case 2i8:
+		if (!IsPlayerInSphere(&twp->pos, 50.0f))
+		{
+			twp->mode = 1i8;
+			twp->cwp->info->attr &= ~0x10u;
+			twp->cwp->info[1].attr |= 0x10u;
+			twp->cwp->info[2].attr |= 0x10u;
+			twp->cwp->info[3].attr |= 0x10u;
+		}
+
+		twp->value.l = AdjustAngle(twp->value.l, 0xC000, 256);
+
+		break;
+	case 3i8:
+		twp->value.l = AdjustAngle(twp->value.l, 0, 256);
+		EntryColliList(twp);
+		break;
+	}
+
+	tp->disp(tp);
+}
+
+static void __cdecl ObjectTPDoubleDoor_r(task* tp)
+{
+	if (multiplayer::IsActive())
+	{
+		ObjectTPDoubleDoor_m(tp);
+	}
+	else
+	{
+		TARGET_STATIC(ObjectTPDoubleDoor)(tp);
 	}
 }
