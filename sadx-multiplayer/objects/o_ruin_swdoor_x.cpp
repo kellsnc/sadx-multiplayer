ba0b505b6fc2c83ff2581b1ab1a48e865425aede
kellsnc <kellspam@outlook.fr>
2022-04-03 12:38:39 +0200

    o_ruin_swdoor_x: cleanup

--- sadx-multiplayer/objects/o_ruin_swdoor_x.cpp
+++ sadx-multiplayer/objects/o_ruin_swdoor_x.cpp
@@ -1,54 +1,52 @@
 #include "pch.h"
+#include "multiplayer.h"
 
-
-void SwDoorX_Main_r(task* tp);
-Trampoline ObjSwDoorX_Main_t(0x5E7660, 0x5E7668, SwDoorX_Main_r);
-void SwDoorX_Main_r(task* tp)
+enum : __int8
 {
-    taskwk* data = tp->twp;
-    NJS_VECTOR a3;
-    char pnum;
+    MODE_NORMAL,
+    MODE_OPEN,
+    MODE_OPENWAIT,
+    MODE_CLOSE,
+    MODE_END
+};
 
-    if ( (CheckRangeOut(tp) ||ObjectSelectedDebug((ObjectMaster*)tp)))
+static void ObjectRuinSwdoorX_m(task* tp)
+{
+    if (CheckRangeOut(tp))
         return;
 
-    if (!data->mode)
+    auto twp = tp->twp;
+
+    if (CheckCollisionCylinderP(&twp->pos, 26.0f, 3.0f))
     {
-        if (CheckCollisionCylinderP(&data->pos, 26.0, 3.0))
+        auto pnum = GetClosestPlayerNum(&twp->pos);
+
+        NJS_POINT3 p;
+        GetPlayerPosition(pnum, 0, &p, 0);
+        if ((p.z - twp->pos.z) * twp->scl.z + (p.x - twp->pos.x) * twp->scl.x > 0.0f)
         {
-            pnum = GetClosestPlayerNum(&data->pos);
-
- 
-            GetBufferedPositionAndRotation(pnum, 0, &a3, 0);
-            if ((a3.z - data->pos.z) * data->scl.z + (a3.x - data->pos.x) * data->scl.x > 0.0)
-            {
-                data->mode = 2;
-                data->wtimer = 0;
-                data->counter.l = 0;
-                data->value.l = 1;
-                PlaySound2(194, data, 1, 0);
-            }
+            twp->mode = MODE_OPENWAIT;
+            twp->wtimer = 0ui16;
+            twp->counter.l = 0;
+            twp->value.l = 1;
+            dsPlay_iloop(194, (int)twp, 1, 0);
         }
+    }
 
-        EntryColliList(data);
-        tp->disp(tp);
-    }
-    else
-    {
-        TaskFunc(origin, ObjSwDoorX_Main_t.Target());
-        origin(tp);
-    }
+    EntryColliList(twp);
+    tp->disp(tp);
 }
 
-void Init_SwDoorX(task* tp)
+static void __cdecl ObjectRuinSwdoorX_r(task* tp);
+Trampoline ObjectRuinSwdoorX_t(0x5E7660, 0x5E7668, ObjectRuinSwdoorX_r);
+static void __cdecl ObjectRuinSwdoorX_r(task* tp)
 {
-    if (multiplayer::IsActive())
+    if (multiplayer::IsActive() && tp->twp->mode == 0)
     {
-        SwDoorX_Main_r(tp);
+        ObjectRuinSwdoorX_m(tp);
     }
     else
     {
-        TaskFunc(origin, ObjSwDoorX_Main_t.Target());
-        origin(tp);
+        TARGET_STATIC(ObjectRuinSwdoorX)(tp);
     }
 }
