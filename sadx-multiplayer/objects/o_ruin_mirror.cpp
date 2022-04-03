df6d8fdfc2430d3e42c37f9f570cece76d960a7b
kellsnc <kellspam@outlook.fr>
2022-04-03 13:18:04 +0200

    o_ruin_mirror: cleanup

--- sadx-multiplayer/objects/o_ruin_mirror.cpp
+++ sadx-multiplayer/objects/o_ruin_mirror.cpp
@@ -1,162 +1,153 @@
 #include "pch.h"
+#include "multiplayer.h"
+#include "camera.h"
 
-TaskFunc(ORmirror, 0x5E2850);
-DataPointer(CCL_INFO, c_colli_mirror, 0x02038C38);
-DataPointer(char, ruin_m_flag, 0x03C75128);
-DataPointer(char, other_flag, 0x03C7512B);
-DataPointer(NJS_VECTOR, MirrorAim_position, 0x02038C28);
-DataPointer(char, name_flag, 0x03C7512A);
-DataPointer(int, nocontimer, 0x03C7512C);
-DataPointer(char, discovery, 0x03C75129);
+#define MIRROR_PNUM(twp) twp->btimer
 
-void __cdecl ORmirror_Main_r(task* task)
+enum : __int8
 {
-    colliwk* colInfo; 
-    unsigned __int16 flag; 
-    _BOOL1 timer; 
-    float cosPosX; 
-    float sinPosZ;
-    NJS_VECTOR smokeRadius; 
-    char pnum;
+    MD_INIT,
+    MD_1,
+    MD_2,
+    MD_3
+};
 
-   taskwk* data = task->twp;
+DataPointer(CCL_INFO, c_colli_mirror, 0x2038C38);
+DataPointer(NJS_POINT3, aim_position, 0x2038C28);
+DataPointer(unsigned __int8, name_flag, 0x3C7512A);
+DataPointer(unsigned __int8, ruin_m_flag, 0x3C75128);
+DataPointer(unsigned __int8, other_flag, 0x3C7512B);
+DataPointer(unsigned __int8, discovery, 0x3C75129);
+DataPointer(int, nocontimer, 0x3C7512C);
+DataPointer(float, max_dist, 0x2038CA0);
 
-    if (!ClipObject((ObjectMaster*)task, 25000000.0))
+static void ObjectRuinMirror_m(task* tp)
+{
+    if (!CheckRangeOutWithR(tp, 25000000.0f))
     {
-        switch (data->mode)
+        auto twp = tp->twp;
+        auto pnum = MIRROR_PNUM(twp);
+
+        switch (twp->mode)
         {
-        case 0:
-            CCL_Init(task, &c_colli_mirror, 1, 4u);
-            task->disp = (TaskFuncPtr)ORmirror_Display;
-            data->timer.w[0] = 0;
-            data->timer.w[1] = 0;
-            data->mode = 1;
+        case MD_INIT:
+            CCL_Init(tp, &c_colli_mirror, 1, 4u);
+            tp->disp = (TaskFuncPtr)0x5E2380;
+            twp->timer.w[0] = 0i16;
+            twp->timer.w[1] = 0i16;
+            twp->mode = MD_1;
             break;
-        case 1:
-            if (data->btimer == 2)
+        case MD_1:
+            if (twp->btimer == 2i8)
             {
-                data->timer.w[1] = 1;
+                twp->timer.w[1] = 1i16;
             }
 
-            if (ruin_m_flag == 1)
+            if (ruin_m_flag == 1ui8)
             {
-                if ((double)(unsigned __int8)other_flag == data->scl.y)
+                auto aim_id = static_cast<unsigned __int8>(twp->scl.y);
+
+                if (other_flag == aim_id)
                 {
-                    data->btimer = 2;
-                    MirrorAim_position.x = data->pos.x;
-                    MirrorAim_position.y = data->pos.y;
-                    MirrorAim_position.z = data->pos.z;
+                    twp->btimer = 2;
+                    aim_position.x = twp->pos.x;
+                    aim_position.y = twp->pos.y;
+                    aim_position.z = twp->pos.z;
                 }
 
-                if ((double)(unsigned __int8)name_flag == data->scl.y && data->timer.w[1] == 1)
+                if (name_flag == aim_id && twp->timer.w[1] == 1i16)
                 {
-                    smokeRadius.x = 0.0;
-                    smokeRadius.y = 0.0;
-                    smokeRadius.z = 0.0;
-                    CreateSmoke(&data->pos, &smokeRadius, 2.0);
+                    NJS_POINT3 velo = { 0.0f, 0.0f, 0.0f };
+                    CreateSmoke(&twp->pos, &velo, 2.0f);
                 }
             }
-            ORmirror_Display((ObjectMaster*)task);
-            colInfo = data->cwp;
-            flag = colInfo->flag;
 
-            if ((flag & 1) != 0)
+            if (twp->cwp->flag & 1)
             {
-                colInfo->flag = flag & 0xFFFE;
+                twp->cwp->flag &= ~1;
 
-                if (data->smode != 1 && data->cwp->hit_cwp->mytask)
+                if (twp->smode != 1 && twp->cwp->hit_cwp->mytask)
                 {
-                    if (data->cwp->hit_cwp->mytask->twp) {
+                    auto hit_tp = twp->cwp->hit_cwp->mytask;
 
-                        pnum = data->cwp->hit_cwp->mytask->twp->counter.b[0];
-                        data->mode = 2;
-                        ORmirror_PositionThing.x = data->pos.x;
-                        ORmirror_PositionThing.y = data->pos.y + 10.0;
-                        ORmirror_PositionThing.z = data->pos.z;
-                        ORmirror_RotationThing.x = 0;
-                        ORmirror_RotationThing.y = data->ang.y;
-                        ORmirror_RotationThing.z = 0;
-                        ForcePlayerAction(pnum, 12);
-                        SetCameraControlEnabled(0);
-                       // CameraSetEventCamera(65, 0);
+                    if (hit_tp->twp)
+                    {
+                        pnum = TASKWK_PLAYERID(hit_tp->twp);
+                        twp->mode = MD_2;
+
+                        SetInputP(pnum, 12);
+
+                        //CamAnyParam.camAnyParamPos.x = twp->pos.x;
+                        //CamAnyParam.camAnyParamPos.y = twp->pos.y + 10.0;
+                        //CamAnyParam.camAnyParamPos.z = twp->pos.z;
+                        //CamAnyParam.camAnyParamAng.x = 0;
+                        //CamAnyParam.camAnyParamAng.y = twp->ang.y;
+                        //CamAnyParam.camAnyParamAng.z = 0;
+                        //SetFreeCameraMode(0);
+                        // CameraSetEventCamera(65, 0);
+
                         PClearSpeed(playermwp[pnum], playerpwp[pnum]);
+                        MIRROR_PNUM(twp) = pnum;
                     }
                 }
             }
             else
             {
-                EntryColliList(data);
+                EntryColliList(twp);
             }
             break;
-        case 2:
-            pnum = data->cwp->hit_cwp->mytask->twp->counter.b[0];
-            ruin_m_flag = 1;
-            ORmirror_Display((ObjectMaster*)task);
-            name_flag = (unsigned __int64)data->scl.x;
-            sinPosZ = data->pos.z - njSin(data->ang.y) * 10.0;
-            cosPosX = njCos(data->ang.y) * 10.0 + data->pos.x;
-            PositionPlayer(pnum, cosPosX, data->pos.y, sinPosZ);
-            data->ang.y = Camera_Data1->Rotation.y + 0x8000;
-            if (discovery == 1)
-            {
- 
-                data->smode = 1;
-                nocontimer = 120;
-                ruin_m_flag = 0;
-                data->mode = 1;
-                discovery = 0;
+        case MD_2:
+            ruin_m_flag = 1ui8;
+            name_flag = static_cast<unsigned __int8>(twp->scl.x);
+            SetPositionP(pnum, twp->pos.x + njCos(twp->ang.y) * 10.0f, twp->pos.y, twp->pos.z - njSin(twp->ang.y) * 10.0f);
 
-                if (data->scl.x == 5.0f)
+            {
+                auto cam_rot = GetCameraAngle(pnum);
+                twp->ang.y = cam_rot ? cam_rot->y + 0x8000 : 0;
+            }
+
+            if (discovery == 1ui8)
+            {
+                twp->smode = 1i8;
+                twp->mode = MD_1;
+                nocontimer = 120;
+                ruin_m_flag = 0ui8;
+                discovery = 0ui8;
+
+                if (twp->scl.x == 5.0f)
                 {
-                    LostWorldFogTarget = -175.0f;
+                    max_dist = -175.0f;
                 }
 
-                CameraReleaseEventCamera();
+                //CameraReleaseEventCamera();
                 dsStop_num(197);
                 SetInputP(pnum, 24);
             }
             break;
-        case 3:
-            timer = ++data->timer.w[0] <= 20;
-            smokeRadius.x = 0.0;
-            smokeRadius.y = 0.0;
-            smokeRadius.z = 0.0;
-
-            if (!timer)
+        case MD_3:
+            if (++twp->timer.w[0] > 20)
             {
-                data->timer.w[0] = 0;
-                CreateSmoke(&data->pos, &smokeRadius, 2.0);
+                twp->timer.w[0] = 0i16;
+                NJS_POINT3 velo = { 0.0f, 0.0f, 0.0f };
+                CreateSmoke(&twp->pos, &velo, 2.0f);
             }
-
-            ORmirror_Display((ObjectMaster*)task);
-            break;
-        default:
             break;
         }
 
-        if (!MissedFrames)
-        {
-            if (ObjectSelectedDebug((ObjectMaster*)task))
-            {
-                DisplayDebugString(1114131, "<-MY ID");
-                DisplayDebugString(1114132, "<-AIM ID");
-            }
-        }
+        tp->disp(tp);
     }
 }
 
-
-static void __cdecl ORmirror_r(task* tp);
-Trampoline ORmirror_t(0x5E2850, 0x5E2855, ORmirror_r);
-static void __cdecl ORmirror_r(task* tp)
+static void __cdecl ObjectRuinMirror_r(task* tp);
+Trampoline ObjectRuinMirror_t(0x5E2850, 0x5E2855, ObjectRuinMirror_r);
+static void __cdecl ObjectRuinMirror_r(task* tp)
 {
-
     if (multiplayer::IsActive())
     {
-        ORmirror_Main_r(tp);
+        ObjectRuinMirror_m(tp);
     }
     else
     {
-        TARGET_STATIC(ORmirror)(tp);
+        TARGET_STATIC(ObjectRuinMirror)(tp);
     }
 }
