7680a8ea80a57cf318159faf52ce49073ba54571
kellsnc <kellspam@outlook.fr>
2022-05-29 03:30:23 +0200

    Patch twinkle_go_round

---
+++ sadx-multiplayer/objects/o_twinkle_go_round.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include "utils.h"
+#include "multiplayer.h"
+
+// Patches the carousel floor to work with multiple players
+
+static void execTPFloor_m(task* tp)
+{
+	auto twp = tp->twp;
+	Angle add_ang = tp->ptp->twp->wtimer;
+
+	twp->ang.y += add_ang;
+
+	CheckDyncolRange(twp, &tp->twp->pos, 200.0f);
+
+	if (twp->flag & 0x100)
+	{
+		for (int i = 0; i < PLAYER_MAX; ++i)
+		{
+			if (CheckPlayerRideOnMobileLandObjectP(i, tp))
+			{
+				// For some reason they did not use forcewk here
+
+				auto ptwp = playertwp[i];
+
+				NJS_POINT3 v;
+				v.x = ptwp->pos.x - twp->pos.x;
+				v.y = 0.0f;
+				v.z = ptwp->pos.z - twp->pos.z;
+				njPushMatrix(_nj_unit_matrix_);
+				njRotateY_(add_ang);
+				njCalcVector(0, &v, &v);
+				njPopMatrixEx();
+				ptwp->pos.x = v.x + twp->pos.x;
+				ptwp->pos.z = v.z + twp->pos.z;
+			}
+		}
+	}
+
+	tp->disp(tp);
+}
+
+static void __cdecl execTPFloor_r(task* tp);
+Trampoline execTPFloor_t(0x6210B0, 0x6210B8, execTPFloor_r);
+static void __cdecl execTPFloor_r(task* tp)
+{
+	if (multiplayer::IsActive() && tp->twp->mode == 1)
+	{
+		execTPFloor_m(tp);
+	}
+	else
+	{
+		TARGET_STATIC(execTPFloor)(tp);
+	}
+}
