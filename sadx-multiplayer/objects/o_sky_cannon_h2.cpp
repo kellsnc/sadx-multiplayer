0c83d38b922f05525aff4b443c4c210d957aba46
kellsnc <kellspam@outlook.fr>
2022-06-01 18:43:18 +0200

    Patch o_sky_talap0, o_sky_cannon_h2

---
+++ sadx-multiplayer/objects/o_sky_cannon_h2.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "utils.h"
+#include "multiplayer.h"
+
+// Big Cannon at the end of act 1
+
+static void __cdecl ObjectSkydeck_cannon_h2_Exec_r(task* tp);
+Trampoline ObjectSkydeck_cannon_h2_Exec_t(0x5F38F0, 0x5F38F5, ObjectSkydeck_cannon_h2_Exec_r);
+static void __cdecl ObjectSkydeck_cannon_h2_Exec_r(task* tp)
+{
+    if (multiplayer::IsActive())
+    {
+        auto backup = playertwp[0];
+        playertwp[0] = playertwp[GetTheNearestPlayerNumber(&tp->twp->pos)];
+        TARGET_STATIC(ObjectSkydeck_cannon_h2_Exec)(tp);
+        playertwp[0] = backup;
+    }
+    else
+    {
+        TARGET_STATIC(ObjectSkydeck_cannon_h2_Exec)(tp);
+    }
+}
--- sadx-multiplayer/objects/o_sky_talap0.cpp
+++ sadx-multiplayer/objects/o_sky_talap0.cpp
@@ -2,11 +2,11 @@
 #include "utils.h"
 #include "multiplayer.h"
 
-static void __cdecl ObjectSkydeck_c_connect_Exec_r(task* tp);
-Trampoline ObjectSkydeck_c_connect_Exec_t(0x5FAE90, 0x5FAE98, ObjectSkydeck_c_connect_Exec_r);
-static void __cdecl ObjectSkydeck_c_connect_Exec_r(task* tp)
+static void __cdecl ObjectSkydeck_c_talap0_Exec_r(task* tp);
+Trampoline ObjectSkydeck_c_talap0_Exec_t(0x5FB5F0, 0x5FB5F8, ObjectSkydeck_c_talap0_Exec_r);
+static void __cdecl ObjectSkydeck_c_talap0_Exec_r(task* tp)
 {
-    TARGET_STATIC(ObjectSkydeck_c_connect_Exec)(tp);
+    TARGET_STATIC(ObjectSkydeck_c_talap0_Exec)(tp);
 
     if (multiplayer::IsActive())
     {
