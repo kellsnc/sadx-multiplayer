b560f9c7dc87bf2125288b77144c124fc963bf28
kellsnc <kellspam@outlook.fr>
2022-06-11 01:13:58 +0200

    Patch o_finalegg_elevator2

--- sadx-multiplayer/sadx_utils.h
+++ sadx-multiplayer/sadx_utils.h
@@ -310,6 +310,7 @@ DataPointer(Angle, cloud_roll, 0x3C7F05C);
 DataPointer(Angle, cloud_pitch, 0x3C7F028);
 FunctionPointer(void, SetUserGravityXZ, (int ang, int angz), 0x43B4C0);
 DataPointer(NJS_OBJECT, object_cl_uki_cl_uki, 0x1A29EEC);
+DataPointer(NJS_OBJECT, object_mdlelv1_cl_elvtr_cl_elvtr, 0x1A3D74C);
 
 static const void* const pLockingOnTargetEnemy2Ptr = (void*)0x7984B0;
 static inline void pLockingOnTargetEnemy2(motionwk2* mwp, taskwk* twp, playerwk* pwp)
