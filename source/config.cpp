e2f7a9f0885df190d525eb594c26b522abc06a82
kellsnc <kellspam@outlook.fr>
2022-06-18 03:50:38 +0200

    Add networking config

--- source/config.cpp
+++ source/config.cpp
@@ -7,6 +7,12 @@ namespace config
 	bool splitScreenEnabled = true;
 	bool indicatorEnabled = false;
 
+	namespace network
+	{
+		std::string default_ip("localhost");
+		int default_port = 27015;
+	}
+
 	void read(const char* path)
 	{
 		const IniFile* config = new IniFile(std::string(path) + "\\config.ini");
@@ -16,6 +22,14 @@ namespace config
 		{
 			splitScreenEnabled = general->getBool("SplitScreen", splitScreenEnabled);
 			indicatorEnabled   = general->getBool("Indicator", indicatorEnabled);
+		}
+
+		const IniGroup* network = config->getGroup("Networking");
+
+		if (network)
+		{
+			network::default_ip = network->getString("DefaultAddress", network::default_ip);
+			network::default_port = network->getInt("DefaultPort", network::default_port);
 		}
 
 		delete config;
--- source/config.h
+++ source/config.h
@@ -5,5 +5,11 @@ namespace config
 	extern bool splitScreenEnabled;
 	extern bool indicatorEnabled;
 
+	namespace network
+	{
+		extern std::string default_ip;
+		extern int default_port;
+	}
+
 	void read(const char* path);
 }
