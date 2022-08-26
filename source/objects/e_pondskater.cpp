d183f8419891678a0b9d4dc11e85f75e522a9a97
kellsnc <kellspam@outlook.fr>
2022-08-27 01:53:39 +0200

    Move EnemyPond crash patch to e_pondskater.cpp

--- source/objects/e_pondskater.cpp
+++ source/objects/e_pondskater.cpp
@@ -2,8 +2,9 @@
 #include "SADXModLoader.h"
 #include "Trampoline.h"
 
-static void __cdecl PondDisplayer_r(task* tp);
-Trampoline PondDisplayer_t(0x7AA3D0, 0x7AA3D5, PondDisplayer_r);
+static FunctionHook<void, task*> PondExecutor_hook(0x7AA870);
+static FunctionHook<void, task*> PondDisplayer_hook(0x7AA3D0);
+
 static void __cdecl PondDisplayer_r(task* tp)
 {
     if (multiplayer::IsActive())
@@ -15,13 +16,36 @@ static void __cdecl PondDisplayer_r(task* tp)
     }
     else
     {
-        TARGET_STATIC(PondDisplayer)(tp);
+		PondDisplayer_hook.Original(tp);
     }
+}
+
+void PondExecutor_r(task* tp)
+{
+	auto twp = tp->twp;
+	auto ewp = (enemywk*)tp->mwp;
+
+	if (twp && ewp)
+	{
+		if (twp->mode < 3)
+		{
+			if (EnemyCheckDamage(twp, ewp))
+			{
+				twp->mode = 3;
+				twp->counter.b[1] = 0;
+				twp->counter.b[2] = 1;
+				twp->wtimer = 0;
+				twp->scl.z = 0.35f;
+				return;
+			}
+		}
+	}
+
+	PondExecutor_hook.Original(tp);
 }
 
 void PatchPondSkater()
 {
-    WriteData<5>((void*)0x7AA627, 0x90ui8); // remove redundant SetVelocityP
-    WriteData<5>((void*)0x7AA7A9, 0x90ui8); // remove redundant SetVelocityP
-    WriteData<5>((void*)0x7AA707, 0x90ui8); // remove redundant SetVelocityP
+	PondDisplayer_hook.Hook(PondDisplayer_r);
+	PondExecutor_hook.Hook(PondExecutor_r);
 }
