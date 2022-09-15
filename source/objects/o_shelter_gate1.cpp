1ae349a5a9a6b5af054367f307de59728a3d3d47
kellsnc <kellspam@outlook.fr>
2022-09-15 14:11:31 +0200

    Fix mistakes in ShelterGate1 patch

--- source/objects/o_shelter_gate1.cpp
+++ source/objects/o_shelter_gate1.cpp
@@ -4,12 +4,20 @@
 
 enum : __int8
 {
+	MODE_INIT,
 	MODE_NORMAL,
 	MODE_OPEN,
 	MODE_WAIT,
 	MODE_CLOSE,
 	MODE_END
 };
+
+static bool CheckCollisionCylinder(NJS_POINT3* pt, NJS_POINT3* vp, float r, float h)
+{
+	NJS_VECTOR v = *pt;
+	njSubVector(&v, vp);
+	return v.x * v.x + v.z * v.z - r * r <= 0.0f && njAbs(v.y) <= h;
+}
 
 static void __cdecl execShelterGate1_r(task* tp); // "ExecATask"
 Trampoline execShelterGate1_t(0x5A1F70, 0x5A1F78, execShelterGate1_r);
@@ -18,20 +26,25 @@ static void __cdecl execShelterGate1_r(task* tp)
 	if (multiplayer::IsActive() && tp->twp->mode == MODE_NORMAL)
 	{
 		auto twp = tp->twp;
-		auto pnum = CheckCollisionCylinderP(&twp->pos, 22.4f, 30.0f) - 1;
 
-		if (pnum >= 0)
+		for (int pnum = 0; pnum < PLAYER_MAX; ++pnum)
 		{
 			NJS_POINT3 v;
 			if (GetPlayerPosition(pnum, 0, &v, 0))
 			{
-				if ((v.z - twp->pos.z) * twp->value.f + (v.x - twp->pos.x) * twp->timer.f > 0.0f)
+				if (CheckCollisionCylinder(&v, &twp->pos, 22.4f, 30.0f))
 				{
-					twp->mode = MODE_OPEN;
-					twp->counter.l = 0;
+					if ((v.z - twp->pos.z) * twp->value.f + (v.x - twp->pos.x) * twp->timer.f > 0.0f)
+					{
+						twp->mode = MODE_OPEN;
+						twp->counter.l = 0;
+						break;
+					}
 				}
 			}
 		}
+
+		tp->disp(tp);
 	}
 	else
 	{
