17d6fd82ada63b3bc3301f537953aacb61ded30e
Sora <sora.x@hotmail.fr>
2022-10-15 15:22:48 +0200

    SD small cannons: Add multiplayer support

---
+++ source/objects/o_sky_cannon_cmn.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+
+//patch multiple cannon in SD to make every player bounce on them
+void EffHodaiBombReflectPlayer_r(taskwk* a1);
+static FunctionHook<void, taskwk*> EffHodaiBombReflectPlayer_t((intptr_t)0x5FE570, EffHodaiBombReflectPlayer_r);
+
+void EffHodaiBombReflectPlayer_r(taskwk* a1)
+{
+	if (multiplayer::IsActive())
+	{
+		if (a1 && a1->cwp && a1->cwp->hit_cwp && a1->cwp->hit_cwp->mytask)
+		{
+			auto data = a1->cwp->hit_cwp->mytask->twp;
+
+			for (uint8_t i = 0; i < multiplayer::GetPlayerCount(); i++)
+			{
+				if (data == playertwp[i])
+				{
+					EnemyBounceThing(i, 0.0, 2.0, 0.0);
+					data->pos.y = data->pos.y + 1.0;
+					return;
+				}
+			}
+		}
+		else
+		{
+			return;
+		}
+	}
+
+	EffHodaiBombReflectPlayer_t.Original(a1);
+}
