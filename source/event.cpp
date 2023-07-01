4bea2adceb8b08747862835b578b88f6a813a7f2
Sora <sora.x@hotmail.fr>
2023-07-01 02:25:11 +0200

    Fix Post SD cutscene crash

--- source/event.cpp
+++ source/event.cpp
@@ -3,14 +3,13 @@
 
 static FunctionHook<void, int> EV_Load2_t(EV_Load2);
 static char backupMode = 0;
+static FunctionHook<void> EV_Exec2_t(EV_Exec2);
 
 static int bannedCutscene[] = { 0x2 };
 void EV_Load2_r(int no)
 {
 	if (multiplayer::IsActive())
 	{
-		backupMode = multiplayer::IsCoopMode() ? 0 : 1; //unused for now
-
 		for (uint16_t j = 0; j < LengthOfArray(bannedCutscene); j++)
 		{
 			if (bannedCutscene[j] == no)
@@ -29,7 +28,7 @@ void EV_Load2_r(int no)
 
 		}
 	}
-
+	
 	return EV_Load2_t.Original(no);
 }
 
@@ -39,10 +38,22 @@ void RecreateMultiPlayer()
 	return RunLevelDestructor(5);
 }
 
+//fix arbitrary crash on some cutscene (race issue)
+void DelayCutsceneStart(int a1)
+{
+	EV_InitPlayer(a1);
+	EV_Wait(5);
+}
+
 void initEvents()
 {
+
 	WriteData<1>((int*)0x42FD98, 0x3); //Load Event Char: change task lvl index from 1 to 3 (fix cutscene char crash, race process weld issue)
 	EV_Load2_t.Hook(EV_Load2_r);
 	WriteJump((void*)0x431286, RecreateMultiPlayer);
 	WriteCall((void*)0x431385, RecreateMultiPlayer);
+	//<--patches-->
+	//patch Post Sky Deck cutscene (race)
+	WriteCall((void*)0x656FE7, DelayCutsceneStart);
+	WriteCall((void*)0x655B66, DelayCutsceneStart);
 }
