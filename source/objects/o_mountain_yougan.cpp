4a933de2e1c697a6aacdfdde49f5983e137f8f48
Kell <kellspam@outlook.fr>
2023-07-27 23:44:42 +0200

    RM Lava: teleport submerged players to the surface, use dyncol

--- source/objects/o_mountain_yougan.cpp
+++ source/objects/o_mountain_yougan.cpp
@@ -2,8 +2,12 @@
 #include "SADXModLoader.h"
 #include "Trampoline.h"
 #include "multiplayer.h"
+#include "sadx_utils.h"
 
-// Patch to allow lava to go both ways and player respawn above it
+// Red Mountain lava adjustments
+// - Lava can now go down
+// - Players below lava level are teleported to the surface
+// - Replaced shape collision with geometry collision
 
 #define TARGET rd_mountain_twp->scl.x
 #define SPEED rd_mountain_twp->scl.y
@@ -12,10 +16,46 @@ static void Object_Mountain_Yougan_m(task* tp)
 {
 	auto twp = tp->twp;
 
-	//twp->pos.y = twp->scl.z = rd_mountain_twp->pos.y = 30.0f;
-
 	switch (twp->mode)
 	{
+	case 0:
+	{
+		twp->mode = 1;
+		tp->disp = (TaskFuncPtr)0x602530;
+		tp->dest = (TaskFuncPtr)0x59DBF0;
+		twp->pos.x = -1468.0f;
+		twp->pos.z = 1559.0f;
+
+		Float height = 28.0f;
+		if (GetPlayerNumber() != Characters_Gamma)
+		{
+			height = 130.0f;
+		}
+		twp->scl.z = height;
+		twp->pos.y = height;
+		rd_mountain_twp->pos.y = height;
+		rd_mountain_twp->scl.x = height;
+
+		// We replace shape collision with a geometry collision, more stable
+		auto object = GetMobileLandObject();
+		if (object)
+		{
+			object->pos[0] = twp->pos.x;
+			object->pos[1] = twp->pos.y;
+			object->pos[2] = twp->pos.z;
+			object->ang[0] = 0;
+			object->ang[1] = 0;
+			object->ang[2] = 0;
+			object->scl[0] = 1.0f;
+			object->scl[1] = 1.0f;
+			object->scl[2] = 1.0f;
+			object->basicdxmodel = ((NJS_OBJECT*)0x2484D5C)->basicdxmodel;
+			RegisterCollisionEntry(ColFlags_Solid | ColFlags_Dynamic | ColFlags_Hurt, tp, object);
+			twp->counter.ptr = object;
+		}
+
+		break;
+	}
 	case 1:
 		twp->pos.z = 1559.0f;
 		twp->pos.x = -1468.0f;
@@ -33,15 +73,8 @@ static void Object_Mountain_Yougan_m(task* tp)
 
 			rd_mountain_twp->pos.y = twp->pos.y;
 			rd_mountain_twp->counter.f = rd_mountain_twp->scl.y;
-
-			if (continue_data.pos.y < twp->pos.y + 10.0f)
-			{
-				continue_data.pos.y = twp->pos.y;
-			}
 		}
-		
-		dsPlay_timer(130, (int)twp, 1, 0, 30);
-		EntryColliList(twp);
+		dsPlay_timer(SE_DM_MAGMAUP, (int)twp, 1, 0, 30);
 		break;
 	case 3:
 	{
@@ -52,9 +85,26 @@ static void Object_Mountain_Yougan_m(task* tp)
 		{
 			twp->mode = 1;
 		}
-		EntryColliList(twp);
 		break;
 	}
+	}
+
+	// Move geometry collision up
+	auto col = (NJS_OBJECT*)twp->counter.ptr;
+	if (col)
+	{
+		col->pos[1] = twp->pos.y;
+	}
+
+	// If some players are below lava level, teleport above
+	for (int i = 0; i < PLAYER_MAX; ++i)
+	{
+		auto ptwp = playertwp[i];
+		if (ptwp && ptwp->pos.y < twp->pos.y - 5.0f)
+		{
+			ptwp->pos.y = twp->pos.y;
+			ptwp->wtimer = 240;
+		}
 	}
 
 	object_youUP_yogun_yogun.basicdxmodel->mats->attr_texId = *(Uint8*)0x3C80F94;
@@ -65,7 +115,7 @@ static void __cdecl Object_Mountain_Yougan_r(task* tp);
 Trampoline Object_Mountain_Yougan_t(0x602670, 0x602676, Object_Mountain_Yougan_r);
 static void __cdecl Object_Mountain_Yougan_r(task* tp)
 {
-	if (multiplayer::IsActive() && tp->twp->mode != 0)
+	if (multiplayer::IsEnabled())
 	{
 		Object_Mountain_Yougan_m(tp);
 	}
