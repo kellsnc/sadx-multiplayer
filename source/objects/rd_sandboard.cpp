27cd34b2ffd68ca9c296c3921490180c8982eb4b
Kell <kellspam@outlook.fr>
2023-08-02 22:33:35 +0200

    Fix board loading in Sand Hill

--- source/objects/rd_sandboard.cpp
+++ source/objects/rd_sandboard.cpp
@@ -32,8 +32,24 @@ static void InitPlayers()
 {
 	for (int i = 0; i < PLAYER_MAX; ++i)
 	{
-		SetInputP(i, PL_OP_SNOWBOARDING);
-		CreateElementalTask(3u, 2, (TaskFuncPtr)0x4959E0);
+		auto ptwp = playertwp[i];
+
+		if (ptwp)
+		{
+			auto char_id = TASKWK_CHARID(ptwp);
+
+			switch (char_id)
+			{
+			case Characters_Sonic:
+				SetInputP(i, PL_OP_SNOWBOARDING);
+				CreateElementalTask(3, 2, (TaskFuncPtr)0x4959E0)->twp->counter.b[0] = i;
+				break;
+			case Characters_Tails:
+				SetInputP(i, PL_OP_SNOWBOARDING);
+				CreateElementalTask(3, 2, (TaskFuncPtr)0x461510)->twp->counter.b[0] = i;
+				break;
+			}
+		}
 	}
 }
 
