5039ceae31fb8a40bfa24387d6e42f4e55c0f6a9
Sora <sora.x@hotmail.fr>
2023-08-19 15:24:11 +0200

    Twinkle Park - Amy: Fix music and act swap in co-op

--- source/levels.cpp
+++ source/levels.cpp
@@ -902,6 +902,16 @@ void __cdecl Casino_StartPos_r(Uint8 pno, float x, float y, float z)
 	}
 }
 
+int __cdecl PatchGetPlayerCharacterName(char index)
+{
+	if (multiplayer::IsActive())
+	{
+		return CurrentCharacter;
+	}
+
+	return GetCharacterID(index);
+}
+
 void InitLevels()
 {
 	// Patch start positions
@@ -945,6 +955,8 @@ void InitLevels()
 
 	// Ice Cap breath generator
 	WriteData((uint8_t*)0x4E91AE, (uint8_t)PLAYER_MAX);
+
+	WriteCall((void*)0x61CB77, PatchGetPlayerCharacterName); //fix Amy character check
 
 	// Speed Highway Act 2 skybox
 	WriteData((taskwk***)0x610765, &camera_twp);
