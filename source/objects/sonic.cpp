c16daddb082b54132a5d0f6c493a87b234695136
Kell <kellspam@outlook.fr>
2023-08-29 00:28:03 +0200

    Patch Light Speed Dash

--- source/objects/sonic.cpp
+++ source/objects/sonic.cpp
@@ -5,18 +5,108 @@
 #include "collision.h"
 #include "sonic.h"
 
+#define SPINTIMER(pwp) (pwp->free.sw[1])
+#define HOMING_TIMER1(pwp) (pwp->free.sw[2])
+#define HOMING_TIMER2(pwp) (pwp->free.sw[3])
+
+FunctionHook<void, taskwk*, motionwk2*, playerwk*> SonicHomingOnRings_t(0x492AB0);
 FunctionHook<taskwk*, taskwk*, playerwk*> SonicCheckLSSTargetEnemy_t(0x492710);
 TaskHook SonicDirectAhead_t((intptr_t)0x493C70);
 TaskHook SonicJiggle_t((intptr_t)Sonic_Jiggle_Main);
 UsercallFunc(Bool, Sonic_CheckInput_t, (playerwk* a1, taskwk* a2, motionwk2* a3), (a1, a2, a3), 0x495FA0, rEAX, rEAX, rEDI, stack4);
 TaskHook SonicTheHedgehog_t(0x49A9B0);
 
+static void SonicHomingOnRings_m(taskwk* twp, motionwk2* mwp, playerwk* pwp)
+{
+	auto pnum = TASKWK_PLAYERID(twp);
+	auto arl = GetTargetRingList(pnum);
+
+	if (!arl->twp)
+	{
+		PGetInertia(twp, mwp, pwp);
+		PGetSpeed(twp, mwp, pwp);
+		return;
+	}
+
+	NJS_VECTOR forward = { 1.0f, 0.0f, 0.0f };
+	PConvertVector_P2G(twp, &forward);
+	njUnitVector(&forward);
+
+	taskwk* tgt_twp = NULL;
+	Float tgt_dist;
+
+	while (arl->twp)
+	{
+		NJS_VECTOR vec = arl->twp->pos;
+		njAddVector(&vec, &arl->twp->cwp->info->center);
+		njSubVector(&vec, &twp->pos);
+		njScalor(&vec);
+
+		if (VectorAngle(&forward, &vec, 0) <= 0x3000 && (!tgt_twp || arl->dist < tgt_dist))
+		{
+			tgt_twp = arl->twp;
+			tgt_dist = arl->dist;
+		}
+
+		++arl;
+	}
+
+	if (tgt_twp)
+	{
+		if (HOMING_TIMER1(pwp) < 5)
+		{
+			HOMING_TIMER1(pwp) = 5;
+		}
+
+		NJS_VECTOR vec = tgt_twp->pos;
+		njAddVector(&vec, &tgt_twp->cwp->info->center);
+		njSubVector(&vec, &twp->pos);
+
+		twp->ang.y = AdjustAngle(twp->ang.y, njArcTan2(vec.z, vec.x), 0x1800);
+		mwp->ang_aim.y = twp->ang.y;
+
+		Float dist = njSqrt(tgt_dist);
+		if (dist > 8.0f)
+		{
+			dist = 8.0f;
+		}
+		else if (dist < 2.0f)
+		{
+			dist = 2.0f;
+		}
+
+		if (njScalor(&vec) <= 1.0f)
+		{
+			vec = mwp->spd;
+		}
+
+		njUnitVector(&vec);
+		mwp->spd.x = vec.x * dist;
+		mwp->spd.y = vec.y * dist;
+		mwp->spd.z = vec.z * dist;
+		PConvertVector_G2P(twp, &vec);
+		pwp->spd = vec;
+	}
+}
+
+static void __cdecl SonicHomingOnRings_r(taskwk* twp, motionwk2* mwp, playerwk* pwp)
+{
+	if (multiplayer::IsActive())
+	{
+		SonicHomingOnRings_m(twp, mwp, pwp);
+	}
+	else
+	{
+		SonicHomingOnRings_t.Original(twp, mwp, pwp);
+	}
+}
+
 static taskwk* SonicCheckLSSTargetEnemy_m(taskwk* twp, playerwk* pwp)
 {
 	auto pnum = TASKWK_PLAYERID(twp);
 	auto ael = GetTargetEnemyList(pnum);
 
-	pwp->free.sw[3] = 0;
+	HOMING_TIMER2(pwp) = 0;
 
 	if (!ael->twp)
 	{
@@ -144,6 +234,7 @@ static void __cdecl SonicTheHedgehog_r(task* tp)
 
 void initSonicPatch()
 {
+	SonicHomingOnRings_t.Hook(SonicHomingOnRings_r);
 	SonicCheckLSSTargetEnemy_t.Hook(SonicCheckLSSTargetEnemy_r);
 	SonicDirectAhead_t.Hook(SonicDirectAhead_r);
 	SonicJiggle_t.Hook(SonicJiggle_r);
